6b44431f0595242e8c464cac4824c835ca1f0a63
Ronald-patrick <ronald.patrick1101@gmail.com>
2022-10-04 23:29:14 +0530

    Added frontend

---
+++ client/src/components/Theme.js
@@ -0,0 +1,15 @@
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+
+const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: "#022e5a",
+      },
+      secondary: {
+        main: "#022e5a",
+      },
+
+    },
+  });
+  
+  export default theme;
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,7 @@
 var SupplyChainContract = artifacts.require("./SupplyChain.sol");
 var StructureContract = artifacts.require("./Structure.sol")
-// var ManufacturerContract = artifacts.require("./rolesUtils/Manufacturer.sol");
 
 module.exports = function(deployer) {
-  // deployer.deploy(ManufacturerContract);
   deployer.deploy(StructureContract);
   deployer.deploy(SupplyChainContract, {gas: 15555555});
-  // deployer.deploy(FetchContract, {gas: 15555555});
 };
